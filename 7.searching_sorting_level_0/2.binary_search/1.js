b2404f23d933b8f0beb693f434686f1d8740f4b1
Baba0608 <babafakruddinnitturu@gmail.com>
2025-06-04 23:44:40 +0530

    binary search

---
+++ 7.searching_sorting_level_0/2.binary_search/1.js
@@ -0,0 +1,14 @@
+function binarySearch(nums, target) {
+  let i = 0,
+    j = nums.length - 1;
+
+  while (i <= j) {
+    let mid = Math.floor((i + j) / 2);
+
+    if (nums[mid] === target) return mid;
+    else if (nums[mid] < target) i = mid + 1;
+    else j = mid - 1;
+  }
+
+  return -1;
+}
